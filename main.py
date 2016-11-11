4b4baa4150e48ad0723360baf7d3b98cd0ba80c5
Hunter Heidenreich <hunter.scott.heidenreich@gmail.com>
2016-11-11 14:12:41 -0500

    Refactor course grabbing

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ from lxml import html
 import requests
 
 
-if __name__ == "__main__":
-    page = requests.get('https://duapp2.drexel.edu/webtms_du/app?component=subjectDetails&page=CollegesSubjects&service=direct&sp=ZH4sIAAAAAAAAAFvzloG1uIhBPjWlVC%2BlKLUiNUcvs6hErzw1qSS3WC8lsSRRLyS1KJcBAhiZGJh9GNgTk0tCMnNTSxhEfLISyxL1iwtz9EECxSWJuQXWPgwcJUAtzvkpQBVCEBU5iXnp%2BsElRZl56TB5l9Ti5EKGOgamioKCEgY2IwNDM2NToJHBBSBVCoGliUVAZQqGZrqG5gCfPyshpgAAAA%3D%3D&sp=SAS&sp=SENGL&sp=1')
+def print_courses(courses):
+    for course in courses:
+        print(course)
+
+
+def load_subject(subject_url):
+    page = requests.get(subject_url)
     tree = html.fromstring(page.content)
 
     courselist = tree.xpath('//tr[@class="even"]/td/text() | //tr[@class="odd"]/td/text()')
     crns = tree.xpath('//tr[@class="even"]/td/p/a/text() | //tr[@class="odd"]/td/p/a/text()')
     capacity = tree.xpath('//tr[@class="even"]/td/p/attribute::title | //tr[@class="odd"]/td/p/attribute::title')
+
     courses = []
     for x in range(len(courselist)):
         if x % 11 == 0:
@@ -18,5 +24,12 @@ if __name__ == "__main__":
         elif x % 11 == 9:
             courselist[x] = capacity[x // 11]
         courses[len(courses) - 1].append(courselist[x])
-    for course in courses:
-        print(course)
+    return courses
+
+
+if __name__ == "__main__":
+    main_url = 'https://duapp2.drexel.edu/webtms_du/app?component=subjectDetails&page=CollegesSubjects&service=direct&sp=ZH4sIAAAAAAAAAFvzloG1uIhBPjWlVC%2BlKLUiNUcvs6hErzw1qSS3WC8lsSRRLyS1KJcBAhiZGJh9GNgTk0tCMnNTSxhEfLISyxL1iwtz9EECxSWJuQXWPgwcJUAtzvkpQBVCEBU5iXnp%2BsElRZl56TB5l9Ti5EKGOgamioKCEgY2IwNDM2NToJHBBSBVCoGliUVAZQqGZrqG5gCfPyshpgAAAA%3D%3D'
+    subjects2017 = {
+        'ENGL': '&sp=SAS&sp=SENGL&sp=1'
+    }
+    print_courses(load_subject(main_url + subjects2017['ENGL']))
