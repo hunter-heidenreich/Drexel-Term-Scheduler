a7121fd9917abfef4c97d646db65cedb94f9134d
Hunter Heidenreich <hunter.scott.heidenreich@gmail.com>
2016-11-11 12:24:07 -0500

    Populating list with full classes and substituting the CRNs

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ if __name__ == "__main__":
     page = requests.get('https://duapp2.drexel.edu/webtms_du/app?component=subjectDetails&page=CollegesSubjects&service=direct&sp=ZH4sIAAAAAAAAAFvzloG1uIhBPjWlVC%2BlKLUiNUcvs6hErzw1qSS3WC8lsSRRLyS1KJcBAhiZGJh9GNgTk0tCMnNTSxhEfLISyxL1iwtz9EECxSWJuQXWPgwcJUAtzvkpQBVCEBU5iXnp%2BsElRZl56TB5l9Ti5EKGOgamioKCEgY2IwNDM2NToJHBBSBVCoGliUVAZQqGZrqG5gCfPyshpgAAAA%3D%3D&sp=SAS&sp=SENGL&sp=1')
     tree = html.fromstring(page.content)
     #This will create a list of buyers:
-    evens = tree.xpath('//tr[@class="even" | @class="odd"]/td/text()')
+    courselist = tree.xpath('//tr[@class="even"]/td/text() | //tr[@class="odd"]/td/text()')
+    crns = tree.xpath('//tr[@class="even"]/td/p/a/text() | //tr[@class="odd"]/td/p/a/text()')
     courses = []
-    for x in range(len(evens)):
+    for x in range(len(courselist)):
         if x % 11 == 0:
             courses.append([])
-        #elif x % 11 == 6:
-        #    print(tree.xpath('//tr[@class="even"]/td/p/a/text()'))
-        courses[len(courses) - 1].append(evens[x])
+        elif x % 11 == 6:
+            courselist[x] = crns[x // 11]
+        courses[len(courses) - 1].append(courselist[x])
     for course in courses:
         print(course)
